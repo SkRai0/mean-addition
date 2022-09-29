1c4619bbc63eff75c3eb5831ae83fe5abf7c003c
123vaibhav-123 <vaibhav_202000449@smit.smu.edu.in>
2022-09-29 18:45:35 +0530

    First

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'My Calculator';
+
+  count = 5;
+
+  listOfNumbers = [0,1,2,3,4,5]
+  listOfNames = []
+  number1=0
+  number2=0
+  sum = 0
+  add = () => {
+    this.sum = this.number1+this.number2
+  }
+
+
 }
